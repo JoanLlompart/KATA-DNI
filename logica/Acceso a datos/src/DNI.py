89ea2909a1800cdc7fa50e383968c606f9821aea
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2022-01-10 12:42:40 +0100

    codigo no funcional

--- logica/Acceso a datos/src/DNI.py
+++ logica/Acceso a datos/src/DNI.py
@@ -1,4 +1,4 @@
-from dni import DNI
+from DNI import DNI
 import pytest
 
 class DNI:
@@ -9,5 +9,12 @@ class DNI:
     letras_dni = list('T R W A G Y F P D X B N J Z S Q V H L C K E'.replace('\t','').replace(' ', ''))
 
     DNI = input('Inserta tu DNI: ')
+    DNI_numero = int(DNI[0:-1])
+    dni_letra = DNI[-1]
 
-    DNI % len(letras_dni)
+        letra_posicion = DNI_numero % len(letras_dni)
+
+        if dni_letra == letras_dni[letra_posicion]:
+            print('Su dni es correcto')
+        else:
+            print('DNI incorrecto')
---
+++ logica/Acceso a datos/src/test/test_dni.py
@@ -0,0 +1,6 @@
+from DNI import DNI
+import pytest
+
+@pytest.mark.DNI
+def test_out_Letter():
+    assert ""
